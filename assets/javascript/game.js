aa421229a80b519052f3db72dcc0bdfd20be8373
Monica Nguyen <Monica.Nguyen@tesseractsensors.com>
2017-07-31 16:55:57 -0400

    more progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,8 +14,7 @@ var gameWon = 1;
 // Wins=0 and a win++ at the end of the win loop
 // guessLimit=6 and a guess-- at the end of guess loop
 var wins = 0;
-var losses = 0;
-//guess limit= head, body, arm, arm, leg, leg
+//guessLimit= head, body, arm, arm, leg, leg
 var guessLimit = 6;
 
 //need to generate a random word
@@ -27,93 +26,88 @@ var travelWordArray = randomWord.split("");
 console.log("travelWordArray" , travelWordArray);
 // need an array of -- to put on the screen that is the same size as the word
 var travelWordLength = travelWordArray.length;
+
+
 //for loop to put in the same number of - as letters
 for (var i = 0; i < travelWordArray.length; i++) {
 	displayArray.push("_ ");
 }
+
+
 console.log("display letter" , displayArray);
 console.log("bad letter" , badLetter);
 
+
 // Function is run whenever a key is pressed
 document.onkeyup = function(event) {
-	while (gameWon > -1 && guesslimit >= 0){
-	//Determines which key is pressed
-	var userGuess = event.key;
-
-		//if alpha is pressed then run code
-			//make alpha into lower case userguess.tolowercase()
-
-	//gives index of -1 if userGuess is not in any of the 3 arrays
-	var wordArrayTest = travelWordArray.indexOf(userGuess);
-	var displayArrayTest = displayArray.indexOf(userGuess);
-	var badLetterTest = badLetter.indexOf(userGuess);
 	var gameWon = displayArray.indexOf("_ ");
+	//while (gameWon > -1 && guessLimit >= 0){
+
+		//Determines which key is pressed
+		var userGuess = event.key;
+
+			//if alpha is pressed then run code
+				//make alpha into lower case userguess.tolowercase()
+
+		//gives index of -1 if userGuess is not in any of the 3 arrays
+		var wordArrayTest = travelWordArray.indexOf(userGuess);
+		var displayArrayTest = displayArray.indexOf(userGuess);
+		var badLetterTest = badLetter.indexOf(userGuess);
 
 
+		//want to take the userGuess and find out if its in one of the 3 arrays
+		//A- if all 3 arrays return a -1 then push letter in badletterarray and decrement my guesslimit by 1
+			if (wordArrayTest == -1 && displayArrayTest == -1 && badLetterTest == -1) {
+				badLetter.push(userGuess);
+				guessLimit--;
 
-	//A- if all 3 arrays return a -1 then push letter in badletterarray and decrement my guesslimit by 1
-		if (wordArrayTest == -1 && displayArrayTest == -1 && badLetterTest == -1) {
-			badLetterTest.push(userGuess);
-			guesslimit--;
-		}
+				if (gameWon == -1) {
+					console.log("You Win!");
+					wins++;
+				}
 
-	//B- if dsplayarraytest > -1 || badletterTest >-1, then do nothing (becasue it is already on the screen)
-		//don't write anything billy tricks....
-
-	//C -while wordarraytest = x and x > -1, displayArray[x] = wordArray[x] , THEN we need to get rid of the letter by  
-	//   assigning Wordarray[x] = "*", then retest wordArrayTest = travelWordArray.indexOf(userGuess);
-	// word ******      diaplay S I l l Y     badaRRAY  X H O 
-
-	// 	while wordarraytest = x and x > -1{
-	// 		displayArray[x] = wordArray[x];
-	// 		Wordarray[x] = "*";
-	// 		wordArrayTest = travelWordArray.indexOf(userGuess);
-	// 	}
-
-	// when display array is filled then they win, increment win++
-	//restart game with new word
-
-	gameWon = displayArray.indexOf("_ ");
+				if (guessLimit == 0) {
+					console.log("You Loose... ");
 }
-	console.log("userGuess", userGuess);
-	console.log("wordArrayTest" , wordArrayTest);
-	console.log("displayArrayTest" , displayArrayTest);
-	console.log("badLetterTest", badLetterTest);
-	console.log("GameWon?", gameWon);
+			}
 
 
-	//want to take the userGuess and find out if its in one of the 3 arrays
-	//will return -1 if the userGuess ,  not in one of the 3 arrays
-	//if userGuess is P and P is in travelwordarray then show P in unguessedletter
-	// for (var i = 0; i < travelWordArray.length; i++){
-	// 	//if (){ 
-	// 		displayArray.push(travelWordArray[i]);
-	// 	}
-	// 	//if userGuess is A and A is not in travelwordarry then show A in bad letter
-	// 	if (userGuess !== travelWordArray[i] && userGuess === displayArray[i] && userGuess === badLetter[i]){
-	// 		badLetter.push(userGuess);	
-	// 	}
-	}
+		//B- if dsplayarraytest > -1 || badletterTest >-1, then do nothing (because it is already on the screen)
+		//don't write anything here just does it for you. 
+
+		//C -while wordarraytest = x and x > -1, displayArray[x] = wordArray[x] , THEN we need to get rid of the letter by  
+		//   assigning Wordarray[x] = "*", then retest wordArrayTest = travelWordArray.indexOf(userGuess);
+			while (wordArrayTest > -1) {
+				var x = wordArrayTest;
+				displayArray[x] = travelWordArray[x];
+				travelWordArray[x] = "*";
+				wordArrayTest = travelWordArray.indexOf(userGuess);
+			}
+
+		// when display array is filled then they win, increment win++
+
+
+		//restart game with new word
+
+		gameWon = displayArray.indexOf("_ ");
+		console.log("userGuess", userGuess);
+		console.log("wordArrayTest" , wordArrayTest);
+		console.log("displayArrayTest" , displayArrayTest);
+		console.log("badLetterTest", badLetterTest);
+		console.log("GameWon?", gameWon);
+		console.log("travelWordArray", travelWordArray);
+		console.log("displayArray", displayArray);
+		console.log("badLetter", badLetter);
+		console.log("guessLimit", guessLimit);
+		console.log("wins", wins);
+	//}
+}
 
 
 
-
-
-// print --- for word by number in array
-// store badLetter into an array
-// if correct key pressed show letter and if all letters are shown, you win, win++
-
-	//if travelWords[i][j] == 
-
-// if wrong display wrong letter here
-// if already guessed display letter here
-// deduct # of guessLimit from guessLimit--
-// count # of numberGuesses, numberGuesses ++
 // if numberGuesses = guessLimit, you loose
 // if input is invalid do not do anything until it is valid
 // Game over
-
-// Html keeps track of score
 
 	// Set inner html contents of #hamgman
 	//document.querySelector("#Hangman").innerHTML = html;
